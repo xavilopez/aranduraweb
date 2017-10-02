c94db2f899371c627022f8870879bbc45884ecba
WilliamsGon <williamseduardo93@gmail.com>
2017-10-01 23:17:28 -0300

    Se modificó para agregar Botstrap y una pantalla Index de ejemplo

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
+//
+// app.get('/', function (req, res) {
+//   res.send('PUTO EL QUE LEE')
+// })
+//
+// app.listen(3000, function () {
+//   console.log('servidor arriba')
+// })
+var express = require('express');
+var routes = require('./routes/index');
+var cookieParser = require('cookie-parser');
 
-app.get('/', function (req, res) {
-  res.send('PUTO EL QUE LEE')
-})
+var app = express();
 
-app.listen(3000, function () {
-  console.log('servidor arriba')
-})
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(cookieParser());
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', routes);
+
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+		message: err.message,
+		error: err
+	});
+});
+
+app.listen(8000);
